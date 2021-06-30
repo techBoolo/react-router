51c342dd8edd17f0266216280ac02e84af813018
techboolo <teklufa@gmail.com>
2021-06-30 12:07:52 +0300

    styling with react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import { Switch, Route, Link, useHistory, Redirect, useRouteMatch } from 'react-router-dom';
 import { useCounter, useField } from './customHook';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Table, Form, Button, Alert, Navbar, Nav } from 'react-bootstrap';
 import './App.css';
 
 const notes = [
@@ -14,15 +16,27 @@ const Users = () => <h3>U</h3>
 
 const Notes = ({ notes }) => {
   return (
-      <ul>
-        { notes.map(note => 
-            <li key={note.id}>
-              <Link to={`/notes/${note.id}`}>{note.content}</Link>
-            </li>
-          )
-        }
-      </ul>
-      )
+      <div>
+        <h4>N</h4>
+        <Table striped>
+        <tbody>
+          <ul>
+            { notes.map(note => 
+                <tr key={note.id}>
+                  <td>
+                    <Link to={`/notes/${note.id}`}>{note.content}</Link>
+                  </td>
+                  <td>
+                    { note.content }
+                  </td>
+                </tr>
+              )
+            }
+          </ul>
+        </tbody>
+        </Table>
+      </div>
+    )
 }
 
 const Note = ({ note }) => {
@@ -35,22 +49,25 @@ const Login = ({ handleSubmit }) => {
   return (
     <div>
       <h3>Li</h3>
-      <form onSubmit={handleSubmit}>
-          <div>
-            Nm: <input />
-          </div>
-          <div>
-            P: <input type='password'/>
-          </div>
-          <button type='submit'>Li</button>
-        </form>
-      </div>
-    )
+      <Form onSubmit={handleSubmit}>
+        <Form.Group>
+            <div>
+              <Form.Label>Nm:</Form.Label> <Form.Control type='text' name='username' />
+            </div>
+            <div>
+              <Form.Label>P:</Form.Label> <Form.Control type='password'/>
+            </div>
+            <Button variant='primary' type='submit'>Li</Button>
+        </Form.Group>
+      </Form>
+    </div>
+  )
 }
 
 function App() {
   const [ user, setUser ] = useState(null);
   const [ name, setName ] = useState('');
+  const [ message, setMessage ] = useState(null);
   const bdate = useField('date');
   const height = useField('number');
 
@@ -67,6 +84,10 @@ function App() {
   const handleSubmit = (ev) => {
     ev.preventDefault(); 
     setUser(ev.target[0].value);
+    setMessage(`Welcome ${ev.target[0].value}`);
+    setTimeout(() => {
+      setMessage(null) 
+    }, 5000)
     history.push('/');
   }
   const padding = { padding: 5 }
@@ -74,16 +95,36 @@ function App() {
   const border = { borderBottom: "2px solid #ccc" }
 
   return (
-    <div className="App">
-        <div>
-          <Link to='/'  style={padding}>H</Link>
-          <Link to='/notes'  style={padding}>N</Link>
-          <Link to='/users' style={padding}>U</Link>
-          { user
-          ? <em>{ user } logged in</em>
-          : <Link to='/login' style={padding}>login</Link>
-          }
-        </div>
+    <div className="container">
+      {
+        (message && 
+         <Alert variant='success'>
+          { message }
+         </Alert>)
+      }
+        <Navbar collapseOnSelect expand='lg' bg='dark' variant='dark'>
+        <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+        <Navbar.Collapse id='responsive-navbar-nav'>
+          <Nav className='mr-auto'>
+                <Nav.Link href='#' as='span'>
+                  <Link to='/'  style={padding}>H</Link>
+                </Nav.Link>
+                <Nav.Link href='#' as='span'>
+                  <Link to='/notes'  style={padding}>N</Link>
+                </Nav.Link>
+                <Nav.Link href='#' as='span'>
+                <Link to='/users' style={padding}>U</Link>
+                </Nav.Link>
+                <Nav.Link href='#' as='span'>
+                  { user
+                    ? <em>{ user } logged in</em>
+                    : <Link to='/login' style={padding}>login</Link>
+                  }
+                </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+
         <Switch>
           <Route path='/notes/:id'><Note note={note} /></Route>
           <Route path='/users'>
